cf0fd2266a09cf38845a24c2e73ecdd12fce5dd3
hoolev <hoolev@163.com>
2014-11-11 16:25:39 +0800

    使用spawn来执行命令

--- scripts/generate-kit.sh
+++ scripts/generate-kit.sh
@@ -14,12 +14,27 @@ cd $DEVKIT_DIR
 git clone https://github.com/jubolin/meteor.git
 cd meteor
 git checkout jubo-master
-./scripts/generate-dev-bundle.sh
-./meteor --help
+wget https://d3sqy0vbqsdhku.cloudfront.net/dev_bundle_Linux_i686_0.3.61.tar.gz
+./meteor create --example todos
+cd todos 
+../meteor --port 22786 > meteor.out &
+echo "building meteor ..."
+while true
+do
+    str=$(tail -1 meteor.out | awk '{print $3}');
+    if [ "$str" = "running" ]; then
+        pid=$(ps aux | grep meteor/tools/main.js | grep -v "grep" | awk '{print $2}')
+        kill -9  $pid
+        break
+    fi
+done 
+
+cd -
+rm -rf todos
 cd $DEVKIT_DIR
 
 # Prepare Demeteorizer
-git clone https://github.com/onmodulus/demeteorizer.git
+git clone https://github.com/jubolin/demeteorizer.git
 cd demeteorizer
 git checkout v2.0.4
 npm install
@@ -30,6 +45,8 @@ cd $DEVKIT_DIR
 # Prepare Qemu
 
 # Finish
-
+cd $WORK_DIR  
+sudo ln -s "$WORK_DIR/jubo" "/usr/local/bin/jubo"  
+echo "Generate kit finish."
 
 
